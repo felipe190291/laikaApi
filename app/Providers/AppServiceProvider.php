5b2f88db9abb6ce2326c38ecf4c5ff15329d4ffa
felipe190291 <pipe_cesped@hotmail.com>
2022-06-20 21:34:08 -0500

    w

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register any application service.
      *
      * @return void
      */
